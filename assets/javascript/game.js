c214a110797dfae89497abf60a416cd876f518cc
sandond77 <sandu@ucdavis.edu>
2017-11-10 23:34:22 -0800

    Completed!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
+// Declaring global variables
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
 var dogs = ["husky", "corgi", "beagle", "poodle", "shiba", "bulldog"];
-var mammals = ["aardvark", "elephant", "dolphin", "monkey", "rabbit", "manatee"];
-var animals = ["chameleon", "penguin", "jellyfish", "leopard", "salamander", "chinchila"]
+var mammals = ["gorilla", "elephant", "dolphin", "monkey", "rabbit", "manatee"];
+var animals = ["chameleon", "penguin", "jellyfish", "leopard", "kangaroo", "chinchilla"]
 var seed = Math.floor((Math.random() * 6)); //all my word arrays have a length of 6; could also use array.length
 var blanks = [];
 var currentWord = dogs[seed].split("");
@@ -22,90 +23,9 @@ var repeatLetter;
 var useDogs = true; 
 var useMammals
 var useAnimals
-
-
-underscorer(currentWord); //this will preload the word as the page is loaded
-
-// Registers the key pressed and also converts capitalized letters to lower case
-document.onkeyup = function(){
-  lastGuess = event.key.toLowerCase();
-  letterChecker(lastGuess);
-
-  if (gameActive === true && isLetter === true && repeatLetter !== true ){
-    console.log("the last guess is " + lastGuess);
-    contains = currentWord.includes(lastGuess);
-
-  //uses boolean to check to see if the guessed letter is in the word
-  //Since the displayed underscores were joined as a string, 
-  //they must be split back into elements of an array for easy update of each letter. 
-  //The array is then turned back into a string
-  //If there isnt a second index, then [indexTwo]=[index] 
-    if (contains===true) {
-      index = currentWord.indexOf(lastGuess)
-      indexTwo = currentWord.lastIndexOf(lastGuess);
-      blanks = blanks.split("");
-      blanks[index] = lastGuess;
-      blanks[indexTwo] = lastGuess; 
-      blanks = blanks.join("");
-      idCurrentWord.innerHTML = blanks;
-
-      if (blanks===dogs[seed]) {
-      alert("Congrats! You have guessed the word correctly. Hit the New Game button to play again!");
-      wins += 1;
-      idWinCount.innerHTML = wins;
-      console.log("wins: "+wins);
-      gameActive = false;
-      }
-
-      
-    }else {
-      lives -= 1;
-      idLives.innerHTML = lives;
-      var addGuessedLetter = document.createTextNode(lastGuess);
-      idGuess.appendChild(addGuessedLetter);
-
-      if (lives === 0) {
-      alert("You lose! You have run out of guesses. Hit the New Game button to play again!");
-      losses += 1;
-      idLoseCount.innerHTML = losses;
-      idCurrentWord.innerHTML = dogs[seed]; //this will reveal the current word when the player losses
-      console.log("losses: "+losses);
-      gameActive = false;
-      } 
-    }
-  } else if (isLetter === false){
-    alert("You did not enter a letter of alphabet. Try another key")
-  } else if (gameActive === false){
-    alert("Start a New Game to play again")
-  } else if (repeatLetter === true){
-    alert("You have already used this letter. Try another letter")
-  }
-}
-
-//provides reset button with functionality
-document.getElementById("reset").onclick = function(){reset()};
-
-// //provides diffiulty buttons with functionality
-// document.getElementById("easy").onclick = function(){
-// useDogs = true;
-// useMammals = false;
-// useAnimals = false;
-// underscorer(currentWord);
-// };
-
-// document.getElementById("normal").onclick = function(){
-// useDogs = false;
-// useMammals = true;
-// useAnimals = false;
-// underscorer(currentWord);
-// };
-
-// document.getElementById("hard").onclick = function(){
-// useDogs = false;
-// useMammals = false;
-// useAnimals = true;
-// underscorer(currentWord);
-// };
+var status
+var audioWin = new Audio("assets/javascript/wow.mp3")
+var audioLose = new Audio("assets/javascript/lose.mp3")
 
 //this function will check if the input is an alphabet letter when called upon. 
 //If the input is an alphabet letter, it will check to make sure that the letter hasnt been used yet.
@@ -137,7 +57,7 @@ function letterChecker(arg){
 //It then calculates a new seed to choose a different 
 //while resetting the # of lives to 10 and clears the guessed letters
 function reset(){
-  if (lives < 10){
+  if (lives < 10 && status !== "won" ){
   losses += 1;
   idLoseCount.innerHTML = losses;
   }
@@ -145,12 +65,20 @@ function reset(){
   blanks = [];
   usedLetter = [];
   seed = Math.floor((Math.random() * 6));
-  console.log(dogs[seed]);
   lives = 10;
-  currentWord = dogs[seed];
+
+  if (useDogs === true){
+    currentWord = dogs[seed].split("");
+  } else if (useMammals === true){
+    currentWord = mammals[seed].split("");
+  } else if(useAnimals === true){
+    currentWord = animals[seed].split("");
+  }
+
   underscorer(currentWord);
   idGuess.innerHTML = "";
   gameActive = true;
+  status = "playing";
 }
 
 //this function creates an array of blank based on the length of the current word. 
@@ -165,4 +93,92 @@ function underscorer(input){
   idLives.innerHTML = lives;
 }
 
-if (use)
+
+
+
+// Main Game Code//
+underscorer(currentWord); //this will preload the word as the page is loaded
+// Registers the key pressed and also converts capitalized letters to lower case
+document.onkeyup = function(){
+  lastGuess = event.key.toLowerCase();
+  letterChecker(lastGuess);
+
+  if (gameActive === true && isLetter === true && repeatLetter !== true ){
+    contains = currentWord.includes(lastGuess);
+
+  //uses boolean to check to see if the guessed letter is in the word
+  //Since the displayed underscores were joined as a string, 
+  //they must be split back into elements of an array for easy update of each letter. 
+  //The array is then turned back into a string
+  //If there isnt a second index, then [indexTwo]=[index] 
+    if (contains===true) {
+      index = currentWord.indexOf(lastGuess)
+      indexTwo = currentWord.lastIndexOf(lastGuess);
+      blanks = blanks.split("");
+      blanks[index] = lastGuess;
+      blanks[indexTwo] = lastGuess; 
+      blanks = blanks.join("");
+      idCurrentWord.innerHTML = blanks;
+
+      if (blanks===currentWord.join("")) {
+      audioWin.play();
+      alert("Congrats! You have guessed the word correctly. Start a new game or choose another diffiulty!");
+      wins += 1;
+      status = "won";
+      idWinCount.innerHTML = wins;
+      gameActive = false;
+      }
+
+      
+    }else {
+      lives -= 1;
+      idLives.innerHTML = lives;
+      var addGuessedLetter = document.createTextNode(lastGuess);
+      idGuess.appendChild(addGuessedLetter);
+
+      if (lives === 0) {
+      audioLose.play();
+      alert("You lose! You have run out of guesses. Start a new game or choose another diffiulty!");
+      losses += 1;
+      status = "lost";
+      idLoseCount.innerHTML = losses;
+      currentWord = currentWord.join("");
+      idCurrentWord.innerHTML = currentWord; //this will reveal the current word when the player losses
+      gameActive = false;
+      } 
+    }
+  } else if (isLetter === false){
+    alert("You did not enter a letter of alphabet. Try another key")
+  } else if (gameActive === false){
+    alert("Start a new game or choose another diffiulty")
+  } else if (repeatLetter === true){
+    alert("You have already used this letter. Try another letter")
+  }
+}
+
+
+//provides reset button with functionality
+document.getElementById("reset").onclick = function(){reset()};
+
+//provides diffiulty buttons with functionality
+document.getElementById("easy").onclick = function(){
+useDogs = true;
+useMammals = false;
+useAnimals = false;
+reset();
+};
+
+document.getElementById("normal").onclick = function(){
+useDogs = false;
+useMammals = true;
+useAnimals = false;
+reset();
+};
+
+document.getElementById("hard").onclick = function(){
+useDogs = false;
+useMammals = false;
+useAnimals = true;
+reset();
+};
+
